973fa85d2a9c062a8c8caa2f0d7d5e6c89ec4e42
Priyanshtailor07 <priyanshtailor113@gmail.com>
2026-05-19 23:44:47 +0530

    middleware working fine

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,9 +3,18 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+
+
 const app = express()
+
+app.use(cors({origin:'http://localhost:5173'
+}));
+app.use(express.json());
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+
+
+
 
 const io = new Server(server, {
   cors: {
@@ -17,31 +26,37 @@ const io = new Server(server, {
 
 
 io.use((socket, next) => {
-  const token = socket.handshake.auth.token;    
-    if (socket.userId==token) {
-       
-         next();
-    }
-    else {
-        next(new Error("Authentication error: Missing Token"));
-    }
-   socket.userId = token;
+  const token = socket.handshake.auth.token;
+
+  if (!token) {
+    console.log("❌ Gatekeeper: No token found. Slamming the door!");
+    // CRITICAL: You must return next(error) so execution STOPS here
+    return next(new Error("Authentication error: Missing Token"));
+  }
+
+  socket.userId = token;
   next();
-});
+})
 
 
 io.on('connection', (socket) => {
    const userId=socket.userId;
-   const userRoom=`room:${socket.userId}`;
+   const userRoom=`room:${userId}`;
    socket.join(userRoom);
    console.log(`User verified and locked into room: ${userRoom}`);
+
+
+
+   socket.on('disconnect', () => {
+    console.log(`❌ Client disconnected [ID: ${socket.id}] from room: ${userRoom}`);
+  });
 
 });
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('LivePitch Backend is running');
 });
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
